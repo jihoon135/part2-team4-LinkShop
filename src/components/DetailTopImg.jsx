0f46e17802c1d9966b456b1bc63771a88398946c
prkhaeun <hani00415@naver.com>
2025-03-15 16:21:45 +0900

    feat : 상세페이지 상단 이미지 UI 생성

---
+++ src/components/DetailTopImg.jsx
@@ -0,0 +1,9 @@
+import styles from "./Top.module.css";
+
+export const DetailTopImg = () => {
+  return (
+    <div className={styles.topContainer}>
+      <img src="Property 1=Variant3.png" alt="Top 이미지" className={styles.topImage} />
+    </div>
+  );
+};
