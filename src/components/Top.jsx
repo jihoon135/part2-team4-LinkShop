16bb32ae182358b988cca66006aa0772dfe0b47d
prkhaeun <hani00415@naver.com>
2025-03-15 10:40:52 +0900

    Delete src/components/Top.jsx

--- src/components/Top.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Top.module.css";
-
-export const Top = () => {
-  return (
-    <div className={styles.topContainer}>
-      <img src="Property 1=Variant3.png" alt="Top 이미지" className={styles.topImage} />
-    </div>
-  );
-};
