e6c479791791e23e444406a1471b24c45dedc345
yujin0408 <aszx5849@naver.com>
2025-03-17 16:11:38 +0900

    feat: 임시 라우터 설정, 공통 css 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom';
 import DetailPage from './pages/DetailPage/DetailPage.jsx';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <DetailPage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/list" replace />} />
+        {/* <Route path="/list" element={<MainPage />} /> */} {/* 추후 메인페이지 추가 시 수정 필요 */}
+        <Route path=":urlId" element={<DetailPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
